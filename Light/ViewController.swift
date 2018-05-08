13227d2d2b7e427943e774b848aa529d2d03a3cb
張心禹 <kevin@2001-b400-e70d-2b0f-9c3a-8b21-d5be-6a3b.emome-ip6.hinet.net>
2018-05-08 11:39:36 +0800

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by kevin on 2018/5/8.
+//  Copyright © 2018 KevinChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
