9729c137518f87f551efa84993393d1681df7bb1
張心禹 <kevin@2001-b400-e70d-2b0f-0814-1864-3df8-3f38.emome-ip6.hinet.net>
2018-05-08 12:23:34 +0800

    finfished

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -7,11 +7,38 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    let redColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+    let orangeColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+    let yelloColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+    let greenColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+    let blueColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+    let indigoColor = #colorLiteral(red: 0.2941176471, green: 0, blue: 0.5098039216, alpha: 1)
+    let purpleColor = #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1)
+    let colorDistribution = GKRandomDistribution(lowestValue: 0, highestValue: 6)
+    var colorArray = [UIColor]()
+    
+    
+    @IBAction func tapGesturePressed(_ sender: Any) {
+        if count <= 5{
+            count += 1
+            view.backgroundColor = colorArray[count]
+        }else if count == 6{
+            count = 0
+            view.backgroundColor = colorArray[count]
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorArray += [redColor, orangeColor, yelloColor, greenColor, blueColor, indigoColor, purpleColor]
+        count = colorDistribution.nextInt()
+        view.backgroundColor = colorArray[count]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
